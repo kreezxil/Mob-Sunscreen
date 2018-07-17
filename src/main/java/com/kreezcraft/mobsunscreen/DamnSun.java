e68aa109e2a7e158bf06226e0b7e8529049c7474
kreezxil <kreezxil@gmail.com>
2018-07-17 17:08:52 -0500

    1.12.2-1.0.1
    First Release. Skeletons and Zombies no longer burn in sunlight!

--- src/main/java/com/kreezcraft/mobsunscreen/DamnSun.java
+++ src/main/java/com/kreezcraft/mobsunscreen/DamnSun.java
@@ -1,32 +1,46 @@
 package com.kreezcraft.mobsunscreen;
 
+import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.item.EntityBoat;
+import net.minecraft.entity.monster.EntityHusk;
+import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.monster.EntitySkeleton;
 import net.minecraft.entity.monster.EntityZombie;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.EntityEvent;
+import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.event.world.BlockEvent;
+import net.minecraftforge.event.world.BlockEvent.BreakEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 @Mod.EventBusSubscriber
 public class DamnSun {
 
 	@SubscribeEvent
-	public static EntityEvent theSunlight(EntityEvent e) {
-		Entity mob = e.getEntity();
-		if (mob != null)
-			if (mob instanceof EntitySkeleton || mob instanceof EntityZombie) {
-				World world = mob.getEntityWorld();
-				if (mob.getPosition() != null)
-					if (world != null && world.canSeeSky(mob.getPosition()) && mob.isBurning()) {
-
+	public static void entityUpdate(LivingUpdateEvent event) {
+		Entity mob;
+		if (event.getEntity() instanceof EntityZombie || event.getEntity() instanceof EntitySkeleton) {
+			mob = event.getEntity();
+			World world = mob.getEntityWorld();
+			boolean day = world.isDaytime();
+			BlockPos blockpos = mob.getPosition();
+			if (world.canSeeSky(blockpos))
+				if (day) {
+					if (mob.isBurning() && !mob.isInLava())
 						mob.extinguish();
-						System.out.println("Sunscreen!");
-						e.setCanceled(true);
-					}
-			}
-		return e;
+				}
+		}
 	}
+
 }
