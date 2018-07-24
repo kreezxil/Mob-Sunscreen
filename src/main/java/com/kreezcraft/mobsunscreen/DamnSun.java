d293706f972daab6ad2c9d5bc67a90c0e88ed875
kreezxil <kreezxil@gmail.com>
2018-07-24 14:39:21 -0500

    1.12.2-2.0.2
    Added config options.
    You can now specify mobs explicity using their full resource location of modid:unlocalizedname or you by just their modid.
    
    You can get this information by hitting F3+H while in game, then pull in a spawn egg for the creature and look at it in your inventory.

--- src/main/java/com/kreezcraft/mobsunscreen/DamnSun.java
+++ src/main/java/com/kreezcraft/mobsunscreen/DamnSun.java
@@ -1,13 +1,18 @@
 package com.kreezcraft.mobsunscreen;
 
+import java.util.Arrays;
+
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityList;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityBoat;
 import net.minecraft.entity.monster.EntityHusk;
 import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.monster.EntitySkeleton;
 import net.minecraft.entity.monster.EntityZombie;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
@@ -22,6 +27,7 @@ import net.minecraftforge.event.world.BlockEvent.BreakEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 @Mod.EventBusSubscriber
@@ -29,18 +35,34 @@ public class DamnSun {
 
 	@SubscribeEvent
 	public static void entityUpdate(LivingUpdateEvent event) {
-		Entity mob;
-		if (event.getEntity() instanceof EntityZombie || event.getEntity() instanceof EntitySkeleton) {
-			mob = event.getEntity();
-			World world = mob.getEntityWorld();
+		if (event.getEntity() instanceof EntityPlayer)
+			return;
+		String fullId = EntityList.getKey(event.getEntity()).toString();
+		String id[] = fullId.split(":");
+		if (Arrays.asList(Settings.sunscreenSettings.mobList).contains(fullId)
+				|| Arrays.asList(Settings.sunscreenSettings.modList).contains(id[0])) {
+			World world = event.getEntity().getEntityWorld();
 			boolean day = world.isDaytime();
-			BlockPos blockpos = mob.getPosition();
+			BlockPos blockpos = event.getEntity().getPosition();
 			if (world.canSeeSky(blockpos))
 				if (day) {
-					if (mob.isBurning() && !mob.isInLava())
-						mob.extinguish();
+					if (event.getEntity().isBurning() && !event.getEntity().isInLava())
+						event.getEntity().extinguish();
 				}
 		}
 	}
 
+	// System.out.println("Mob found: "+EntityList.getKey(event.getEntity()));
+	// if (event.getEntity() instanceof EntityZombie || event.getEntity() instanceof
+	// EntitySkeleton) {
+	// mob = event.getEntity();
+	// World world = mob.getEntityWorld();
+	// boolean day = world.isDaytime();
+	// BlockPos blockpos = mob.getPosition();
+	// if (world.canSeeSky(blockpos))
+	// if (day) {
+	// if (mob.isBurning() && !mob.isInLava())
+	// mob.extinguish();
+	// }
+	// }
 }
