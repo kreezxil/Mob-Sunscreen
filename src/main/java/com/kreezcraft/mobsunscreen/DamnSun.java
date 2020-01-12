0686d4c7917900d8c6784d248ffea8e21c812c8e
Kreezxil <kreezxil@gmail.com>
2020-01-12 11:07:15 -0600

    1.14.4-2.0.2
    removed the config because I don't know how to set that in 1.14.4, ie, string arrays in the configs.
    
    Also, having this mod installed simply means burnable mobs in the sunlight not in lava are now immune to fire.
    
    I'll bring the config back later once I come up with a solution that actually works.

--- src/main/java/com/kreezcraft/mobsunscreen/DamnSun.java
+++ src/main/java/com/kreezcraft/mobsunscreen/DamnSun.java
@@ -1,68 +1,28 @@
 package com.kreezcraft.mobsunscreen;
 
-import java.util.Arrays;
-
-import net.minecraft.client.renderer.entity.RenderLiving;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityList;
-import net.minecraft.entity.EntityLiving;
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.item.EntityBoat;
-import net.minecraft.entity.monster.EntityHusk;
-import net.minecraft.entity.monster.EntityMob;
-import net.minecraft.entity.monster.EntitySkeleton;
-import net.minecraft.entity.monster.EntityZombie;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.EntityEquipmentSlot;
-import net.minecraft.item.ItemStack;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraftforge.event.entity.EntityEvent;
-import net.minecraftforge.event.entity.living.LivingDamageEvent;
-import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
-import net.minecraftforge.event.entity.living.LivingHurtEvent;
-import net.minecraftforge.event.world.BlockEvent;
-import net.minecraftforge.event.world.BlockEvent.BreakEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent;
-import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
 @Mod.EventBusSubscriber
 public class DamnSun {
 
 	@SubscribeEvent
 	public static void entityUpdate(LivingUpdateEvent event) {
-		if (event.getEntity() instanceof EntityPlayer)
+		if (event.getEntity() instanceof PlayerEntity)
 			return;
-		String fullId = EntityList.getKey(event.getEntity()).toString();
-		String id[] = fullId.split(":");
-		if (Arrays.asList(Settings.sunscreenSettings.mobList).contains(fullId)
-				|| Arrays.asList(Settings.sunscreenSettings.modList).contains(id[0])) {
+
 			World world = event.getEntity().getEntityWorld();
 			boolean day = world.isDaytime();
 			BlockPos blockpos = event.getEntity().getPosition();
-			if (world.canSeeSky(blockpos))
+			if (world.canBlockSeeSky(blockpos)) {
 				if (day) {
 					if (event.getEntity().isBurning() && !event.getEntity().isInLava())
 						event.getEntity().extinguish();
 				}
 		}
 	}
-
-	// System.out.println("Mob found: "+EntityList.getKey(event.getEntity()));
-	// if (event.getEntity() instanceof EntityZombie || event.getEntity() instanceof
-	// EntitySkeleton) {
-	// mob = event.getEntity();
-	// World world = mob.getEntityWorld();
-	// boolean day = world.isDaytime();
-	// BlockPos blockpos = mob.getPosition();
-	// if (world.canSeeSky(blockpos))
-	// if (day) {
-	// if (mob.isBurning() && !mob.isInLava())
-	// mob.extinguish();
-	// }
-	// }
 }
--- src/main/java/com/kreezcraft/mobsunscreen/MobSunscreen.java
+++ src/main/java/com/kreezcraft/mobsunscreen/MobSunscreen.java
@@ -1,43 +1,39 @@
 package com.kreezcraft.mobsunscreen;
 
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.monster.EntitySkeleton;
-import net.minecraft.entity.monster.EntityZombie;
-import net.minecraft.init.Blocks;
-import net.minecraft.world.World;
-import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.Cancelable;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = MobSunscreen.MODID, name = MobSunscreen.NAME, version = MobSunscreen.VERSION)
+@Mod(MobSunscreen.MODID)
 public class MobSunscreen {
 	public static final String MODID = "mobsunscreen";
 	public static final String NAME = "Mob Sunscreen";
 	public static final String VERSION = "@version@";
 
-	@Mod.Instance
 	public static MobSunscreen instance;
 
-	public static Logger logger;
+	private static final Logger LOGGER = LogManager.getLogger();
 
-	@Mod.EventHandler
-	public void preInit(FMLPreInitializationEvent event) {
-		logger = event.getModLog();
+	private void doClientStuff(final FMLClientSetupEvent event) {
 	}
 
-	@Mod.EventHandler
-	public void init(FMLInitializationEvent e) {
+	private void enqueueIMC(final InterModEnqueueEvent event) {
 	}
 
-	@Mod.EventHandler
-	public void postInit(FMLPostInitializationEvent e) {
+	private void processIMC(final InterModProcessEvent event) {
 	}
+	private void setup(final FMLCommonSetupEvent event) {
+	}
+
+	@SubscribeEvent
+	public void onServerStarting(FMLServerStartingEvent event) {
+	}
+
+
 }
